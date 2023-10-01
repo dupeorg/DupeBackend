2cd642a3189359fc6dc9fc8ad519f37720d9e1f8
Divyam Pandya <divyampandya63@gmail.com>
2023-10-01 11:43:52 -0700

    Added Firebase string constants

---
+++ DupeBackend/src/main/java/dupeorg/dupebackend/constants/StringConstants.java
@@ -0,0 +1,7 @@
+package dupeorg.dupebackend.constants;
+
+public class StringConstants {
+    public final static String FIREBASE_PATH_KEY = "C:\\Users\\dpandya4\\Downloads\\divyam_asu_work\\study\\Fall 23\\RobinHood_Hackathon\\DupeBackend\\DupeBackend\\src\\main\\resources\\dupebackend-firebase-adminsdk-x96qj-102424b6d9.json";
+
+    public final static String DATABASE_PATH = "https://dupebackend-default-rtdb.firebaseio.com/";
+}
