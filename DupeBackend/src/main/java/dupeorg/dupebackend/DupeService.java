a53256f0c9d5b4375f9bd052bb1232da47c27bfe
Divyam Pandya <divyampandya63@gmail.com>
2023-10-01 11:44:26 -0700

    Refactored DupeService to add the API calls

--- DupeBackend/src/main/java/dupeorg/dupebackend/DupeService.java
+++ DupeBackend/src/main/java/dupeorg/dupebackend/DupeService.java
@@ -1,15 +1,37 @@
 package dupeorg.dupebackend;
 
+import com.sun.net.httpserver.HttpServer;
 import dupeorg.dupebackend.firebase.FirebaseInitializer;
+import dupeorg.dupebackend.items.ItemAPI;
+import dupeorg.dupebackend.items.ItemService;
+import dupeorg.dupebackend.rest.MainServer;
+
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 
 public class DupeService {
     private final FirebaseInitializer firebaseInitializer;
+    private final ItemService itemService;
+    private final MainServer mainServer;
+
+
 
     public DupeService(){
         this.firebaseInitializer = new FirebaseInitializer();
+        firebaseInitializer.initialize();
+        this.itemService = new ItemService();
+        mainServer = new MainServer(itemService);
     }
 
     void execute(){
-       firebaseInitializer.initialize();
+       try {
+           String res = itemService.createItem("user-1","sofa", "sample_sofa_url", "This is an old sofa");
+           mainServer.createAndStartServer();
+       }
+       catch(Exception e){
+           System.out.println(e.toString());
+       }
+
+
     }
 }
