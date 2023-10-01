80b24ee1404fa33af4c6b1d2aa65645f0a12bec7
Divyam Pandya <divyampandya63@gmail.com>
2023-10-01 15:57:12 -0700

    Pushed TradeRequest code

--- DupeBackend/src/main/java/dupeorg/dupebackend/tradeRequest/TradeRequest.java
+++ DupeBackend/src/main/java/dupeorg/dupebackend/tradeRequest/TradeRequest.java
@@ -1,4 +1,48 @@
 package dupeorg.dupebackend.tradeRequest;
 
 public class TradeRequest {
+
+    private String sender_id;
+    private String sender_item;
+    private String receiver_id;
+    private String receiver_item;
+
+    public TradeRequest(String sender_id, String sender_item, String receiver_id, String receiver_item) {
+        this.sender_id = sender_id;
+        this.sender_item = sender_item;
+        this.receiver_id = receiver_id;
+        this.receiver_item = receiver_item;
+    }
+
+    public String getSender_id() {
+        return sender_id;
+    }
+
+    public void setSender_id(String sender_id) {
+        this.sender_id = sender_id;
+    }
+
+    public String getSender_item() {
+        return sender_item;
+    }
+
+    public void setSender_item(String sender_item) {
+        this.sender_item = sender_item;
+    }
+
+    public String getReceiver_id() {
+        return receiver_id;
+    }
+
+    public void setReceiver_id(String receiver_id) {
+        this.receiver_id = receiver_id;
+    }
+
+    public String getReceiver_item() {
+        return receiver_item;
+    }
+
+    public void setReceiver_item(String receiver_item) {
+        this.receiver_item = receiver_item;
+    }
 }
--- DupeBackend/src/main/java/dupeorg/dupebackend/tradeRequest/TradeRequestService.java
+++ DupeBackend/src/main/java/dupeorg/dupebackend/tradeRequest/TradeRequestService.java
@@ -1,4 +1,46 @@
 package dupeorg.dupebackend.tradeRequest;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.CollectionReference;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.firebase.cloud.FirestoreClient;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
 public class TradeRequestService {
+    private final static String collectionName = "trade_requests";
+
+    public TradeRequestService() {
+    }
+
+    // Create a new trade request and return its unique ID
+    public String createTradeRequest(String sender_id, String sender_item, String receiver_id, String receiver_item)
+            throws ExecutionException, InterruptedException, IOException {
+        Firestore firestore = FirestoreClient.getFirestore();
+        CollectionReference tradeRequestCollection = firestore.collection(collectionName);
+
+        // Create a new trade request object
+        TradeRequest tradeRequest = new TradeRequest(sender_id, sender_item, receiver_id, receiver_item);
+
+        // Add the trade request to Firestore and get its ID
+        DocumentReference docRef = tradeRequestCollection.add(tradeRequest).get();
+        return docRef.getId();
+    }
+
+    // Retrieve all trade requests
+    public List<TradeRequest> getAllTradeRequests() throws ExecutionException, InterruptedException {
+        Firestore firestore = FirestoreClient.getFirestore();
+        CollectionReference tradeRequestCollection = firestore.collection(collectionName);
+
+        ApiFuture<QuerySnapshot> future = tradeRequestCollection.get();
+        QuerySnapshot tradeRequests = future.get();
+
+        return tradeRequests.toObjects(TradeRequest.class);
+    }
+
+    // You can implement methods for updating and deleting trade requests if needed
 }
